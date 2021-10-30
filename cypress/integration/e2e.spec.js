50527764972bc8a8b688e3ec92d17b5ff96d7a70
alexandragonc <alexandrag@outlook.com>
2021-10-30 14:57:38 -0300

    primeiro commit add carrinho

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
-
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dados = require('../fixtures/enderecos.json')
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -12,9 +13,41 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     beforeEach(() => {
         cy.visit('/')
     });
+    it('Adicionar produtos ao carrinho', () => {
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        cy.fixture('produtos').then(produto => {
+            cy.visit('/produtos')
+            cy.addProdutos(produto[0].nome,
+                produto[0].tamanho,
+                produto[0].cor,
+                produto[0].quantidade)
+        });
+        cy.fixture('produtos').then(produto => {
+            cy.visit('/produtos')
+            cy.addProdutos(produto[1].nome,
+                produto[1].tamanho,
+                produto[1].cor,
+                produto[1].quantidade)
+        });
+        cy.fixture('produtos').then(produto => {
+            cy.visit('/produtos')
+            cy.addProdutos(produto[2].nome,
+                produto[2].tamanho,
+                produto[2].cor,
+                produto[2].quantidade)
+        });
+        cy.fixture('produtos').then(produto => {
+            cy.visit('/produtos')
+            cy.addProdutos(produto[3].nome,
+                produto[3].tamanho,
+                produto[3].cor,
+                produto[3].quantidade)
+        });
+        cy.get('.woocommerce-message > .button').click()
+    });
+    
+    it('Preencher dados do checkout', () => {
+        cy.get('.woocommerce-message > .button').click()
     });
 
 
